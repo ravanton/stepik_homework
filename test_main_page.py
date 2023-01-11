c07d383f417d18c44dd81528d3cd94a7d228b3d8
ravanton <antonrekhva@gmail.com>
2023-01-11 10:04:12 +0300

    rewrite the test with a Page Object

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.common.by import By
+from .pages.main_page import MainPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
